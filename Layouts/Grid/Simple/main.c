eabad56c25bbe485353ab18769c059258eba7134
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2015-03-27 02:31:21 +0430

    * grid sample completed

--- Layouts/Grid/Simple/main.c
+++ Layouts/Grid/Simple/main.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <gtk/gtk.h>
 
-void clicked(GtkWidget *widget,gpointer data)
+void clicked(GtkButton *button,gpointer data)
 {
-    g_print("Hello World\n");
+    const gchar *label= gtk_button_get_label(button);
+    g_print("Hello World from %s\n",label);
+}
+
+void close_window(GtkWidget *widget,gpointer data)
+{
+    GtkWidget *window=data;
+    gtk_widget_destroy(window);
 }
 
 static void activate(GtkApplication *app,gpointer user_data)
 {
     GtkWidget *window= gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window),"Grid");
-    g_signal_connect(window,"destroy",G_CALLBACK(gtk_widget_destroy),NULL);
+    g_signal_connect(window,"destroy",G_CALLBACK(close_window),window);
     gtk_container_set_border_width (GTK_CONTAINER (window), 10);
 
     GtkWidget *grid=gtk_grid_new();
@@ -26,7 +33,7 @@ static void activate(GtkApplication *app,gpointer user_data)
     gtk_grid_attach(GTK_GRID(grid),button2,1,0,1,1);
 
     GtkWidget *button3=gtk_button_new_with_label("Quit");
-    g_signal_connect(button3,"clicked",G_CALLBACK(gtk_main_quit),NULL);
+    g_signal_connect(button3,"clicked",G_CALLBACK(close_window),window);
     gtk_grid_attach(GTK_GRID(grid),button3,0,1,2,1);
 
     gtk_widget_show_all(window);
@@ -39,35 +46,4 @@ int main(int argc,char **argv)
     int status= g_application_run(G_APPLICATION(app),argc,argv);
     return status;
 }
-
-/**<
-int main(int argc,char **argv)
-{
-    GtkWidget *window=gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    GtkWidget *grid=gtk_grid_new();
-    GtkWidget *button1=gtk_button_new_with_label("Button1");
-    GtkWidget *button2=gtk_button_new_with_label("Button2");
-    GtkWidget *button3=gtk_button_new_with_label("Quit");
-
-    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
-
-    gtk_window_set_title(GTK_WINDOW(window),"Grid");
-    g_signal_connect(window,"destroy",G_CALLBACK(gtk_main_quit),NULL);
-
-    g_signal_connect(button1,"clicked",G_CALLBACK(clicked),NULL);
-    g_signal_connect(button2,"clicked",G_CALLBACK(clicked),NULL);
-    g_signal_connect(button3,"clicked",G_CALLBACK(gtk_main_quit),NULL);
-
-    gtk_container_add(GTK_CONTAINER(window),grid);
-    gtk_grid_attach(GTK_GRID(grid),button1,0,0,1,1);
-    gtk_grid_attach(GTK_GRID(grid),button1,1,0,1,1);
-    gtk_grid_attach(GTK_GRID(grid),button1,0,1,2,1);
-
-    gtk_widget_show_all(window);
-
-    gtk_main();
-
-    return 0;
-}
- */
 
